8eff9d1169d8df761b2e73a3b5f70d796c1aee86
Energy <energystar.0124@gmail.com>
2017-01-07 19:08:36 +0800

    add example usage

--- downloader.rb
+++ downloader.rb
@@ -10,6 +10,8 @@ filename
 ...
 
 Example usage:
+ruby downloader.rb T:\GoogleDrive\HTMLslides\HTMLslides\html_slides-updated\9.1\audio-list.log T:\GoogleDrive\HTMLslides\HTMLslides\html_slides-updated\9.1\audio
+ruby downloader.rb T:\GoogleDrive\HTMLslides\HTMLslides\html_slides-updated\9.2\audio-list.log T:\GoogleDrive\HTMLslides\HTMLslides\html_slides-updated\9.2\audio
 ruby downloader.rb T:\GoogleDrive\HTMLslides\HTMLslides\html_slides-updated\9.3\audio-list.log T:\GoogleDrive\HTMLslides\HTMLslides\html_slides-updated\9.3\audio
 
 =end
